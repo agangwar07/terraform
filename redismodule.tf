4dc42e757f78f535933f6902dd4ebd72f42c3c20
agangwar07 <80001165+agangwar07@users.noreply.github.com>
2021-05-30 19:58:48 +0530

    redis

---
+++ redismodule.tf
@@ -0,0 +1,36 @@
+module "redis" {
+  source = "umotif-public/elasticache-redis/aws"
+  version = "~> 1.5.0"
+
+  name_prefix           = "core-example"
+  number_cache_clusters = 2
+  node_type             = "cache.t3.small"
+
+  engine_version           = "5.0.6"
+  port                     = 6379
+  maintenance_window       = "mon:03:00-mon:04:00"
+  snapshot_window          = "04:00-06:00"
+  snapshot_retention_limit = 7
+
+  automatic_failover_enabled = true
+
+  apply_immediately = true
+  family            = "redis5.0"
+  description       = "Test elasticache redis."
+
+  subnet_ids = aws_subnet.myvpc-private.id
+  vpc_id     = aws_vpc.myvpc.id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  parameter = [
+    {
+      name  = "repl-backlog-size"
+      value = "16384"
+    }
+  ]
+
+  tags = {
+    Project = "Test"
+  }
+}
