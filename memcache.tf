4e632d3147f673f35d01b2a95258847870ad2992
agangwar07 <80001165+agangwar07@users.noreply.github.com>
2021-05-30 20:15:41 +0530

    redis

--- memcache.tf
+++ memcache.tf
@@ -1,10 +1,10 @@
 resource "aws_elasticache_cluster" "my-cluter" {
   cluster_id           = "cluster-my-cluster"
   engine               = "memcached"
-  node_type            = "cache.m4.large"
+  node_type            = "cache.t3.small"
   num_cache_nodes      = 1
   port                 = 11211
-  subnet_ids           = [ aws_subnet.myvpc-private.id,aws_subnet.myvpc-private1.id ]
+  subnet_group_name = aws_subnet.myvpc-private
   security_group_ids = [ aws_security_group.My_VPC_Security_Group.id ]
   
   tags = {
