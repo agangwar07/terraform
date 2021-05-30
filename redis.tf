6de873bd1dbbd4e742b9a98e0c7cb2e56357ef72
agangwar07 <80001165+agangwar07@users.noreply.github.com>
2021-05-30 20:07:44 +0530

    redis

---
+++ redis.tf
@@ -0,0 +1,16 @@
+resource "aws_elasticache_cluster" "my-redis" {
+  cluster_id           = "cluster-my-redis"
+  engine               = "redis"
+  node_type            = "cache.m4.large"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis3.2"
+  engine_version       = "3.2.10"
+  port                 = 6379
+  security_group_ids = [ aws_security_group.My_VPC_Security_Group.id ]
+  subnet_ids           = [ aws_subnet.myvpc-private.id,aws_subnet.myvpc-private1.id ]
+
+  tags = {
+   Name = "memcache cluster"
+}
+
+}
